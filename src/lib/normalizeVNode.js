1cdc5b53b065988e5ff6de4562dff1343406bfdb
HYOJIN <hyo.khj@gmail.com>
2025-07-13 23:40:22 +0900

    feat: normalizevNode 함수 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,36 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  // 컴포넌트 정규화
+  if (vNode && typeof vNode.type === "function") {
+    const props = { ...vNode.props };
+    props.children = vNode.children;
+    const componentResult = vNode.type(props);
+    return normalizeVNode(componentResult);
+  }
+
+  if (typeof vNode === "object") {
+    const normalizedChildren = [];
+
+    for (const child of vNode.children) {
+      const normalizedChild = normalizeVNode(child);
+      if (normalizedChild !== "") {
+        normalizedChildren.push(normalizedChild);
+      }
+    }
+
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: normalizedChildren,
+    };
+  }
+
   return vNode;
 }
