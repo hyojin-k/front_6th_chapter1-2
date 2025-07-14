4903185a32e9d331070dd84921e6802474d6739b
HYOJIN <hyo.khj@gmail.com>
2025-07-15 01:50:49 +0900

    feat: createElement 함수 작성

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,50 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === undefined || vNode === null || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode.toString());
+  }
+
+  // 배열 타입 처리
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    for (const child of vNode) {
+      const childElement = createElement(child);
+      if (childElement) {
+        fragment.appendChild(childElement);
+      }
+    }
+    return fragment;
+  }
+
+  // 중첩 처리
+  if (typeof vNode === "object") {
+    const element = document.createElement(vNode.type);
+    updateAttributes(element, vNode.props);
+
+    for (const child of vNode.children) {
+      const childElement = createElement(child);
+      if (childElement) {
+        element.appendChild(childElement);
+      }
+    }
+
+    return element;
+  }
+}
+
+function updateAttributes($el, props) {
+  if (!props) return;
+
+  for (const key in props) {
+    if (key === "className") {
+      $el.className = props[key];
+    } else {
+      $el.setAttribute(key, props[key]);
+    }
+  }
+}
